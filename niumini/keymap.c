a567766d834cc9558bd53df64e04c54f5f5f4129
John Mee <git.commit@johnmee.com>
2021-09-09 21:17:41 +1000

    Third layer

--- niumini/keymap.c
+++ niumini/keymap.c
@@ -6,6 +6,7 @@ enum planck_layers {
   _QWERTY,
   _L1,
   _L2,
+  _L3,
   _R1,
   _R2,
   _ADJUST
@@ -90,35 +91,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,     KC_W,      KC_E,      KC_R,      KC_T,     KC_Y,     KC_U,   KC_I,      KC_O,    KC_P,    KC_BSPC,
   KC_TAB,   KC_A,     KC_S,      KC_D,      KC_F,      KC_G,     KC_H,     KC_J,   KC_K,      KC_L,    KC_SCLN, KC_ENT,
   SFTLOCK,  KC_Z,     KC_X,      KC_C,      KC_V,      KC_B,     KC_N,     KC_M,   KC_COMM,   KC_DOT,  KC_SLSH, SFTLOCK,
-  KC_LCTL,  KC_LALT,  KC_LGUI, TT(_L2),  TT(_L1),    KC_SPC,            MO(_R1),   MO(_R2),  KC_LGUI,  KC_RALT, KC_RCTL
+  KC_LCTL,  KC_LALT,  TT(_L3), TT(_L2),  TT(_L1),    KC_SPC,            MO(_R1),   MO(_R2),  KC_LGUI,  KC_RALT, KC_RCTL
 ),
 
 [_L1] = LAYOUT_planck_mit(
   KC_GRV,     C(KC_Z),    C(KC_X),  C(KC_C),    C(KC_V),      XXXXXXX,     _______,   KC_PGUP,    KC_UP,         KC_PGDN,    _______,  _______,
   _______,    KC_LGUI,    KC_LALT,  KC_LCTL,    KC_LSFT,      XXXXXXX,     KC_HOME,   KC_LEFT,    KC_DOWN,       KC_RGHT,    KC_END,   _______,
-  G(S(KC_F)), G(S(KC_ESC)), C(KC_ESC), WM_LCENTER, WM_RCENTER, XXXXXXX,   _______,   C(KC_LEFT), A(S(KC_COMM)), C(KC_RGHT), _______,  _______,
-  _______,    G(KC_R),  G(KC_C),  XXXXXXX,   XXXXXXX,      KC_BSPC,                _______,    _______,       _______,    _______,  _______
+  XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   _______,   C(KC_LEFT), A(S(KC_COMM)), C(KC_RGHT), _______,  _______,
+  _______,    _______,    _______,  XXXXXXX,   XXXXXXX,      KC_BSPC,                _______,    _______,       _______,    _______,  _______
 ),
 
 [_L2] = LAYOUT_planck_mit(
- _______,      _______,   _______,   _______,   _______,    _______,     _______, KC_7,    KC_8,    KC_9,    KC_PAST, _______,
- G(KC_PGUP),   _______,   _______,   _______,   _______,    _______,     _______, KC_4,    KC_5,    KC_6,    KC_MINS, _______,
- G(KC_PGDN),   _______,   A(KC_F12), _______,   _______,    _______,     _______, KC_1,    KC_2,    KC_3,    KC_PPLS, _______,
- G(C(KC_ESC)), _______,   _______,   _______,   _______,    _______,              KC_0,    KC_0,    KC_DOT,  KC_EQL,  _______
+ _______,    _______,   _______,   _______,   _______,          _______,     _______, KC_7,    KC_8,    KC_9,    KC_PAST, _______,
+ _______,    _______,   _______,   _______,   _______,          _______,     _______, KC_4,    KC_5,    KC_6,    KC_MINS, _______,
+ _______,    _______,   _______,  _______,    _______,          _______,     _______, KC_1,    KC_2,    KC_3,    KC_PPLS, _______,
+ _______,    _______,   _______,   _______,   _______,          _______,              KC_0,    KC_0,    KC_DOT,  KC_EQL,  _______
+),
+
+[_L3] = LAYOUT_planck_mit(
+ _______,    _______,   _______,   _______,   _______,          _______, _______, KC_F7,   KC_F8,  KC_F9, _______, _______,
+ _______,    _______,   _______,   _______,   _______,          _______, _______, KC_F4,   KC_F5,  KC_F6, _______, _______,
+ _______,    _______,   _______,   _______,   _______,          _______, _______, KC_F1,   KC_F2,  KC_F3, _______, _______,
+ _______,    _______,   XXXXXXX,   _______,   _______,          _______,          KC_F10, KC_F11, KC_F12, _______, _______
 ),
 
 [_R1] = LAYOUT_planck_mit(
   KC_TILD,    S(KC_1),    S(KC_2),    S(KC_3),  S(KC_4),    S(KC_5),  S(KC_6),    S(KC_7),    S(KC_8),     KC_MINS,     KC_PLUS, _______,
   _______,   S(KC_QUOT),  KC_QUOT,    KC_UNDS,  KC_MINS,     KC_EQL,  _______,    X(MDASH),   X(SMILE),    X(THUMB),    KC_PIPE, _______,
   _______,    KC_LPRN,    KC_LBRC,    KC_LCBR,  KC_PLUS,    KC_ASTR,  _______,    _______,    KC_RCBR,     KC_RBRC,     KC_RPRN, KC_BSLS,
-  _______,    _______,    _______,    _______,  _______,    KC_DEL,               _______,    _______,     _______,     _______, _______
+ G(C(KC_ESC)), _______,   _______,    _______,  _______,    KC_DEL,               _______,    _______,     _______,     _______, G(S(KC_ESC))
 ),
 
 [_R2] = LAYOUT_planck_mit(
-  _______, _______,  _______,  _______, _______, _______, _______, KC_F7,   KC_F8,  KC_F9, _______, _______,
-  _______, KC_LGUI,  KC_LALT,  KC_LCTL, KC_LSFT, _______, _______, KC_F4,   KC_F5,  KC_F6, _______, _______,
-  _______, _______,  _______,  _______, _______, _______, _______, KC_F1,   KC_F2,  KC_F3, _______, _______,
-  _______, _______,  _______,  _______, _______, _______,          KC_F10, KC_F11, KC_F12, _______, _______
+  _______, _______,  G(KC_R),   G(KC_PGUP), G(S(KC_F)),     _______, _______, _______, _______, _______, _______, _______,
+  _______, KC_LGUI, WM_LCENTER, G(KC_PGDN), WM_RCENTER,     _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, A(KC_F12), G(KC_C), C(KC_ESC),          _______, _______, _______, _______, _______, _______, _______,
+  _______, _______,  _______,  _______, _______,            _______,          _______, XXXXXXX, _______, _______, _______
 ),
 
 [_ADJUST] = LAYOUT_planck_mit(
