93aa00b73aeb71d1f204a2bc92e7bebded25514a
John Mee <git.commit@johnmee.com>
2021-09-13 10:43:28 +1000

    First big stab at crkbd

--- crkbd/keymap.c
+++ crkbd/keymap.c
@@ -15,62 +15,87 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #include QMK_KEYBOARD_H
 #include <stdio.h>
+
+// Keys
+#define CTL_SPC LCTL_T(KC_SPC)   // tap space, hold ctrl
+#define SFT_ENT RSFT_T(KC_ENT)  // tap enter, hold shift
+#define C_RGHT  C(KC_RGHT)      // control right
+#define C_LEFT  C(KC_LEFT)      // control left
+#define C_CUT   C(KC_X)
+#define C_PSTE  C(KC_V)
+#define C_COPY  C(KC_C)
+#define C_UNDO  C(KC_Z)
+
+// Window Manager
+#define WM_LEFT  G(KC_COMM)
+#define WM_RGHT  G(KC_DOT)
+#define WM_UP    G(KC_PGUP)
+#define WM_DOWN  G(KC_PGDN)
+#define WM_CNTR  G(KC_C)
+#define WM_VIEW  G(KC_TAB)
+#define WM_FLT   G(S(KC_ESC))
+
+
+enum custom_keycodes {
+    WM_RSET = SAFE_RANGE
+};
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, SFT_ENT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+      XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_RALT
+                                          KC_LGUI,   MO(1), KC_LCTL,    CTL_SPC,   MO(2), KC_RALT
                                       //`--------------------------'  `--------------------------'
-
   ),
 
+  // Nav Layer
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
+       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+      KC_LSFT,  C_UNDO,   C_CUT,  C_COPY,  C_PSTE,   WM_UP,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,  WM_FLT, WM_CNTR, WM_LEFT, WM_RGHT, WM_DOWN,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT,   MO(3), KC_RALT
+                                          XXXXXXX, XXXXXXX, XXXXXXX,    _______,   MO(3), _______
                                       //`--------------------------'  `--------------------------'
   ),
 
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT
+                                          _______,   MO(3), _______,    XXXXXXX, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
   [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, WM_RSET,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RGB_TOG, KC_MPLY, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_DOWN,   KC_UP, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RGB_MOD, XXXXXXX, KC_DOWN,   KC_UP, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
+                                          XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   )
 };
 
-#ifdef OLED_DRIVER_ENABLE
+
+
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   if (!is_keyboard_master()) {
     return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
@@ -133,21 +158,6 @@ void oled_render_keylog(void) {
     oled_write(keylog_str, false);
 }
 
-void render_bootmagic_status(bool status) {
-    /* Show Ctrl-Gui Swap options */
-    static const char PROGMEM logo[][2][3] = {
-        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
-        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
-    };
-    if (status) {
-        oled_write_ln_P(logo[0][0], false);
-        oled_write_ln_P(logo[0][1], false);
-    } else {
-        oled_write_ln_P(logo[1][0], false);
-        oled_write_ln_P(logo[1][1], false);
-    }
-}
-
 void oled_render_logo(void) {
     static const char PROGMEM crkbd_logo[] = {
         0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
@@ -166,10 +176,19 @@ void oled_task_user(void) {
     }
 }
 
+// Macros
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
     set_keylog(keycode, record);
   }
+  switch (keycode) {
+    // Reset gnome display manager for paperwm.
+    case WM_RSET:
+      if (record->event.pressed) {
+        tap_code16(A(KC_F2));
+        SEND_STRING(SS_DELAY(700) "r" SS_TAP(X_ENT));
+      }
+      break;
+  }
   return true;
-}
-#endif // OLED_DRIVER_ENABLE
+};
--- crkbd/rules.mk
+++ crkbd/rules.mk
@@ -1,3 +1,27 @@
-BOOTMAGIC_ENABLE = full
-RGBLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = yes
+# Seems to configure all the hardware features.
+# https://beta.docs.qmk.fm/using-qmk/hardware-features
+# https://beta.docs.qmk.fm/developing-qmk/qmk-reference/getting_started_make_guide#rules-mk-options
+
+BOOTMAGIC_ENABLE = no     # Access to the eeprom with a keyboard combo
+MOUSEKEY_ENABLE = no      # Keys to simulate mouse movements
+EXTRAKEY_ENABLE = yes     # System and Audio key control codes
+CONSOLE_ENABLE = no       # Enables debugging; send print messages to hid_listen
+COMMAND_ENABLE = no       # Send "magic" commands via key combo; eg turn NKRO on/off or console debug on/off
+SLEEP_LED_ENABLE = no     # Keyboard lights stay on although computer/cpu is in sleep mode
+NKRO_ENABLE = no          # Tell the OS to expect combinations of more than 6 keys simultaneously
+
+BACKLIGHT_ENABLE = no     # Specify the backlights pin in config.h
+RGBLIGHT_ENABLE = no      # Addressable RGB backlights.
+RGB_MATRIX_ENABLE = no    # There is an RGB LED on every key.
+
+MIDI_ENABLE = no          # MIDI is a sound module. We don't have sound.
+UNICODE_ENABLE = no      # Unicode chars via UC(<code point>); covers most languages and symbols but not emojis.
+UNICODEMAP_ENABLE = no    # Unicode chars via X(map_index); you'll need a map table in a keymap file.
+UCIS_ENABLE = no          # Unicode chars via mnemonic; needs map table in a keymap file.
+AUDIO_ENABLE = no         # A speaker is wired to the C6 pin.
+VARIABLE_TRACE = no       # Debugging/unit-testing trace on variables
+API_SYSEX_ENABLE = no     # Something to do with quantum Sysex API
+KEY_LOCK_ENABLE = yes     # Key locking; artificially hold down a BASIC KEYCODE. 7%.
+SPLIT_KEYBOARD = yes      # Dual MCUs need to work together.
+
+OLED_DRIVER_ENABLE = yes  # An OLED, or two, is attached.
