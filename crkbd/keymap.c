260ca44cbf4a6c4e59b5ef41d3a8504620246320
John Mee <git.commit@johnmee.com>
2022-05-11 09:37:19 +1000

    Center window when going right; comment out all the unused stuff (oled, rgblight)

--- crkbd/keymap.c
+++ crkbd/keymap.c
@@ -26,7 +26,8 @@
 #define WM_SC_TG G(C(KC_ESC))   // Scratch Toggle
 
 enum custom_keycodes {
-    WM_RSET = SAFE_RANGE
+    WM_RSET = SAFE_RANGE,
+    WM_RCTR
 };
 
 enum unicode_names {
@@ -77,11 +78,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      WM_PGUP, C(KC_Z), C(KC_X), C(KC_C), C(KC_V),WM_SC_TG,                      _______, KC_PGUP,  KC_UP,  KC_PGDN, _______, _______,
+      WM_PGUP, C(KC_Z), C(KC_X), C(KC_C), C(KC_V),WM_SC_TG,                      _______, KC_PGUP,  KC_UP,  KC_PGDN, _______,  KC_BRK,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      WM_PGDN, _______, _______, KC_RCTL, KC_RSFT,WM_SC_VW,                      KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,  KC_END, _______,
+      WM_PGDN, _______, KC_LALT, KC_LCTL, KC_LSFT,WM_SC_VW,                      KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,  KC_END, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LGUI, _______, WM_LEFT, WM_CNTR, WM_RGHT, G(KC_R),                      _______,  C_LEFT, _______,  C_RGHT, _______, _______,
+      KC_LGUI, _______, WM_LEFT, WM_CNTR, WM_RCTR, G(KC_R),                      _______,  C_LEFT, _______,  C_RGHT, _______, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
@@ -101,13 +102,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [5] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                      KC_MINS,    KC_7,    KC_8,    KC_9, _______,  KC_DEL,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                      XXXXXXX,    KC_7,    KC_8,    KC_9, KC_MINS,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                         KC_0,    KC_4,    KC_5,    KC_6, _______, KC_RALT,
+        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                      XXXXXXX,    KC_4,    KC_5,    KC_6,  KC_DOT, KC_RALT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_DOT,    KC_1,    KC_2,    KC_3, _______, KC_RSFT,
+      KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL, _______, _______,    _______, _______, KC_RCTL
+                                          KC_LCTL, _______, _______,       KC_0, _______, KC_RCTL
                                       //`--------------------------'  `--------------------------'
   )
 };
@@ -136,85 +137,85 @@ void rgb_matrix_indicators_user(void) {
 }
 #endif
 
-#ifdef RGBLIGHT_ENABLE
-// Light LEDs according to keyboard layer active
-const rgblight_segment_t PROGMEM my_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 12, HSV_ORANGE}
-);
-const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 12, HSV_BLUE}
-);
+//#ifdef RGBLIGHT_ENABLE
+//// Light LEDs according to keyboard layer active
+//const rgblight_segment_t PROGMEM my_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+//    {0, 12, HSV_ORANGE}
+//);
+//const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+//    {0, 12, HSV_BLUE}
+//);
+//
+//// Now define the array of layers. Later layers take precedence
+//const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+//    my_layer0_layer,
+//    my_layer1_layer
+//);
+//
+//void keyboard_post_init_user(void) {
+//    // Enable the LED layers
+//    rgblight_layers = my_rgb_layers;
+//}
+//
+//layer_state_t default_layer_state_set_user(layer_state_t state) {
+//    rgblight_set_layer_state(0, layer_state_cmp(state, 0));
+//    return state;
+//}
+//
+//layer_state_t layer_state_set_user(layer_state_t state) {
+//    rgblight_set_layer_state(0, layer_state_cmp(state, 0));
+//    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
+//    return state;
+//}
+//#endif
 
-// Now define the array of layers. Later layers take precedence
-const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    my_layer0_layer,
-    my_layer1_layer
-);
-
-void keyboard_post_init_user(void) {
-    // Enable the LED layers
-    rgblight_layers = my_rgb_layers;
-}
-
-layer_state_t default_layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(0, layer_state_cmp(state, 0));
-    return state;
-}
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(0, layer_state_cmp(state, 0));
-    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
-    return state;
-}
-#endif
-
-#ifdef OLED_ENABLE
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master()) {
-    return OLED_ROTATION_90;
-  }
-  return rotation;
-}
-
-void oled_render_corne(void) {
-    static const char PROGMEM crkbd_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
-        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
-        0};
-    oled_write_P(crkbd_logo, false);
-}
-
-static void oled_render_qmk(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4,
-        0x00};
-    oled_write_P(qmk_logo, false);
-}
-
-void oled_render_layer_state(void) {
-    switch (get_highest_layer(layer_state)) {
-        case 1:
-            oled_render_qmk();
-            break;
-        case 2:
-            oled_write_ln_P(PSTR("Symbols"), false);
-            break;
-        default:
-            oled_render_corne();
-    }
-}
-
-
-bool oled_task_user(void) {
-    oled_render_layer_state();
-    //oled_render_keylog();
-    //oled_render_logo();
-    return true;
-}
-#endif
+//#ifdef OLED_ENABLE
+//oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+//  if (!is_keyboard_master()) {
+//    return OLED_ROTATION_90;
+//  }
+//  return rotation;
+//}
+//
+//void oled_render_corne(void) {
+//    static const char PROGMEM crkbd_logo[] = {
+//        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
+//        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
+//        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
+//        0};
+//    oled_write_P(crkbd_logo, false);
+//}
+//
+//static void oled_render_qmk(void) {
+//    static const char PROGMEM qmk_logo[] = {
+//        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+//        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+//        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4,
+//        0x00};
+//    oled_write_P(qmk_logo, false);
+//}
+//
+//void oled_render_layer_state(void) {
+//    switch (get_highest_layer(layer_state)) {
+//        case 1:
+//            oled_render_qmk();
+//            break;
+//        case 2:
+//            oled_write_ln_P(PSTR("Symbols"), false);
+//            break;
+//        default:
+//            oled_render_corne();
+//    }
+//}
+//
+//
+//bool oled_task_user(void) {
+//    oled_render_layer_state();
+//    //oled_render_keylog();
+//    //oled_render_logo();
+//    return true;
+//}
+//#endif
 
 // Macros
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -229,6 +230,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         tap_code16(A(KC_F2));
         SEND_STRING(SS_DELAY(700) "r" SS_TAP(X_ENT));
+      }
+      break;
+    // Auto center window when going right with paperwm.
+    case WM_RCTR:
+      if (record->event.pressed) {
+        tap_code16(G(KC_DOT));
+        tap_code16(G(KC_C));
       }
       break;
   }
